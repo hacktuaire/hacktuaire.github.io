5fba6690441f8eaf090861455d20b17a2f033c81
rmaliki <rochd.maliki@telecom-bretagne.eu>
2018-03-22 14:38:27 +0100

    cleaning & setting the project baseline

---
+++ _site/nav_bar.js
@@ -0,0 +1,54 @@
+document.write('\
+<link href="site_libs/font-awesome-4.5.0/css/font-awesome.min.css" rel="stylesheet" />\
+<div class="navbar navbar-default  navbar-fixed-top" role="navigation">\
+  <div class="container">\
+    <div class="navbar-header">\
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">\
+        <span class="icon-bar"></span>\
+        <span class="icon-bar"></span>\
+        <span class="icon-bar"></span>\
+      </button>\
+      <a class="navbar-brand" href="index.html"></a>\
+    </div>\
+    <div id="navbar" class="navbar-collapse collapse">\
+      <ul class="nav navbar-nav">\
+        <li>\
+  <a href="index.html">\
+    <span class="fa fa-home"></span>\
+     \
+    Accueigl\
+  </a>\
+</li>\
+<li>\
+  <a href="index.html">\
+    <span class="fa fa-eye"></span>\
+     \
+    Edition 2018\
+  </a>\
+</li>\
+<li>\
+  <a href="index.html">\
+    <span class="fa fa-cart-plus"></span>\
+     \
+    Prix\
+  </a>\
+</li>\
+<li>\
+  <a href="index.html">\
+    <span class="fa fa-cart-plus"></span>\
+     \
+    Sponsors\
+  </a>\
+</li>\
+      </ul>\
+      <ul class="nav navbar-nav navbar-right">\
+        <li>\
+  <a>\
+    <span class="fa fa-question-circle"></span>\
+     \
+  </a>\
+</li>\
+      </ul>\
+    </div><!--/.nav-collapse -->\
+  </div><!--/.container -->\
+</div><!--/.navbar --><br><br><br><br>');
